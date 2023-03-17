25ec09584dbb2a82ca1a36f511dcbfb27539ef70
Nooshin. S. Hosseini <hosseini7190@gmail.com>
2023-03-17 10:15:12 +0330

    load method

--- src/main/java/UserRepository.java
+++ src/main/java/UserRepository.java
@@ -15,13 +15,33 @@ public class UserRepository {
         }
 
     }
-    /*public void delete(int id){
+    public void delete(int id){
 
     }
 
-    public User load(int id){
+    public User load(int id) {
+        String query = "SELECT * FROM user_account WHERE user_id=?";
+        Connection connection = JDBCConnection.connetToDB();
+        User user = null;
+        try {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+            if (!resultSet.next()){
+                return null;
+            }
+            int userId = resultSet.getInt("user_id");
+            String name = resultSet.getString("username");
+            String pass = resultSet.getString("pass_word");
+            user = new User(name, pass);
+            user.setUesrId(userId);
+        } catch (SQLException e) {
+            System.out.println("not connected");
+            e.printStackTrace();
+        }
+        return user;
+    }
 
-    }*/
     public void Update (User user){
         String query = "UPDATE user_account SET username = ?, pass_word = ? WHERE user_id = ?";
         Connection connection = JDBCConnection.connetToDB();
